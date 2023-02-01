d9d67cf8efd36a8f6bd65b8e30e17597f2ca92e0
rapeti-saikumar <rapetisaikumar9866@gmail.com>
2023-02-01 21:00:32 +0530

    added styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+        temporibus odit fugit praesentium. Sint ratione perspiciatis architecto
+        voluptatem animi cupiditate aperiam optio aliquam. Dolores eius quam
+        ratione velit cumque laboriosam!
+      </p>
     </div>
   );
 }
